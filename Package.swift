1f4b93e01608ed2a3a76a7df5b11b1bd3595408b
Vladimír Čalfa <vladimir.calfa@>
2023-05-09 15:38:25 +0200

    Version v9

--- Package.swift
+++ Package.swift
@@ -7,13 +7,13 @@ let package = Package(
     platforms: [.iOS(.v15)],
     products: [
         .library(name: "MagicKit", targets: ["Magic"]),
-        .library(name: "ToastViewSwift", targets: ["Toast"])
+        .library(name: "ToastViewSwift", targets: ["ToastWrapper"])
     ],
     dependencies: [
         .package(url: "https://github.com/BastiaanJansen/toast-swift", from: "1.3.1")
     ],
     targets: [
         .binaryTarget(name: "Magic", path: "Frameworks/Magic.xcframework"),
-        .target(name: "Toast", dependencies: [.target(name: "Toast")])
+        .target(name: "ToastWrapper", dependencies: [.target(name: "Toast")])
     ]
 )
