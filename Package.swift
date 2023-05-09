1e52567908841558456bd357102970ee19510114
Vladimír Čalfa <vladimir.calfa@>
2023-05-09 16:15:50 +0200

    Works

--- Package.swift
+++ Package.swift
@@ -1,19 +1,19 @@
 // swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
-
+// Works
 let package = Package(
     name: "MagicKit",
     platforms: [.iOS(.v15)],
     products: [
         .library(name: "MagicKit", targets: ["Magic"]),
-        .library(name: "ToastViewSwift", targets: ["ToastWrapper"])
+        .library(name: "ToastViewSwift", type: .dynamic, targets: ["ToastWrapper"])
     ],
     dependencies: [
         .package(url: "https://github.com/BastiaanJansen/toast-swift", from: "1.3.1")
     ],
     targets: [
         .binaryTarget(name: "Magic", path: "Frameworks/Magic.xcframework"),
-        .target(name: "ToastWrapper", dependencies: [.target(name: "Toast")])
+        .target(name: "ToastWrapper", dependencies: [.product(name: "Toast", package: "toast-swift")])
     ]
 )
---
+++ Sources/Toast/Dummy.swift
@@ -0,0 +1,4 @@
+#if TARGET_OS_WATCH
+#warning("Firebase Analytics does not support the watchOS platform")
+#endif
+import Toast
